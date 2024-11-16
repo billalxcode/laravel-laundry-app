0c0e00ce19d5cb0291dbf1ffcddb5563463c35fd
billalxcode <billal.xcode@gmail.com>
2024-11-16 21:31:30 +0700

    add gravatar in models

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,12 @@ namespace App\Models;
 use Filament\Models\Contracts\FilamentUser;
 use Filament\Models\Contracts\HasAvatar;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Storage;
+use LaraZeus\Boredom\Concerns\HasBoringAvatar;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable implements FilamentUser, MustVerifyEmail, HasAvatar
@@ -53,7 +55,7 @@ class User extends Authenticatable implements FilamentUser, MustVerifyEmail, Has
     }
 
     public function getFilamentAvatarUrl(): string|null {
-        return $this->avatar_url ? Storage::url($this->avatar_url) : null;
+        return Storage::exists($this->avatar_url) ? Storage::url($this->avatar_url) : $this->avatar_url;
     }
 
     public function canAccessPanel(\Filament\Panel $panel): bool
