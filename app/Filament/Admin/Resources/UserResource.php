db3439e7532cad27417d99056f74e1ea12588183
billalxcode <billal.xcode@gmail.com>
2024-11-16 21:30:55 +0700

    fix avatar and add eloquent query

--- app/Filament/Admin/Resources/UserResource.php
+++ app/Filament/Admin/Resources/UserResource.php
@@ -7,7 +7,11 @@ use App\Models\User;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\Auth;
 
 class UserResource extends Resource
 {
@@ -16,14 +20,35 @@ class UserResource extends Resource
     protected static ?string $navigationIcon = 'heroicon-o-users';
     protected static ?string $navigationGroup = 'Master Data';
 
+    public static function getEloquentQuery(): Builder
+    {
+        $user = Auth::user();
+
+        return parent::getEloquentQuery()->whereNot('id', $user->id);
+    }
+
     public static function table(Table $table): Table
     {
         return $table
             ->columns([
-                //
-            ])
-            ->filters([
-                //
+                ImageColumn::make('avatar_url')
+                    ->label('')
+                    ->circular(),
+                TextColumn::make('name')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('email')
+                    ->copyable()
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('email_verified_at')
+                    ->formatStateUsing(fn($state) => $state ? 'Verified' : 'Not verified')
+                    ->badge()
+                    ->colors([
+                        'success' => 'verified',
+                        'danger' => 'not verified'
+                    ])
+                    ->sortable()
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
@@ -33,13 +58,6 @@ class UserResource extends Resource
                     Tables\Actions\DeleteBulkAction::make(),
                 ]),
             ]);
-    }
-
-    public static function getRelations(): array
-    {
-        return [
-            //
-        ];
     }
 
     public static function getPages(): array
