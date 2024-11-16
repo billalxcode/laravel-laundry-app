718bda59cbc9ff874e18a679d165b3087c0e0088
billalxcode <billal.xcode@gmail.com>
2024-11-16 21:48:10 +0700

    add models, migrations, factories, and seeders

---
+++ app/Models/Outlet.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Outlet extends Model
+{
+    /** @use HasFactory<\Database\Factories\OutletFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'name',
+        'address',
+        'phone',
+        'maps'
+    ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+}
