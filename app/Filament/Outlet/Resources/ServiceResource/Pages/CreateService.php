6018819c0159b9de099f9f1fa5ca6030a8e2104f
billalxcode <billal.xcode@gmail.com>
2024-11-17 17:30:57 +0700

    add create service

---
+++ app/Filament/Outlet/Resources/ServiceResource/Pages/CreateService.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Filament\Outlet\Resources\ServiceResource\Pages;
+
+use App\Filament\Outlet\Resources\ServiceResource;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Support\Facades\Auth;
+
+class CreateService extends CreateRecord
+{
+    protected static string $resource = ServiceResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $outlet = Auth::user()->outlet;
+
+        $data['outlet_id'] = $outlet->id;
+
+        return $data;
+    }
+
+    public function form(\Filament\Forms\Form $form): \Filament\Forms\Form
+    {
+        return $form->schema([
+            Section::make('Services Information')
+                ->schema([
+                    TextInput::make('name')
+                        ->required(),
+                    TextInput::make('price')
+                        ->prefix('IDR')
+                        ->numeric(),
+                    Select::make('pricing_type')
+                        ->options([
+                            'per_item' => 'Per Item',
+                            'per_kilogram' => 'Per Kilogram',
+                        ])
+                        ->native(false)
+                        ->default('per_item'),
+                ])->columnSpanFull(),
+            Section::make('Description')
+                ->schema([
+                    RichEditor::make('description')
+                ])
+        ]);
+    }
+}
