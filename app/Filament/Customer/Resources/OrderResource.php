c80e06ee36bbbb0606ae76be847c537a7b0a7a33
billalxcode <billal.xcode@gmail.com>
2024-11-17 20:05:06 +0700

    add order resource

---
+++ app/Filament/Customer/Resources/OrderResource.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Filament\Customer\Resources;
+
+use App\Filament\Customer\Resources\OrderResource\Pages;
+use App\Models\Order;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class OrderResource extends Resource
+{
+    protected static ?string $model = Order::class;
+
+    protected static ?string $navigationIcon = 'tabler-basket-filled';
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListOrders::route('/'),
+            'create' => Pages\CreateOrder::route('/create'),
+        ];
+    }
+}
