4e411a7475145d5c1d4fafba14d8a3cd513a6b5a
billalxcode <billal.xcode@gmail.com>
2024-11-17 19:44:34 +0700

    add edit service form

--- app/Filament/Outlet/Resources/ServiceResource/Pages/EditService.php
+++ app/Filament/Outlet/Resources/ServiceResource/Pages/EditService.php
@@ -4,6 +4,10 @@ namespace App\Filament\Outlet\Resources\ServiceResource\Pages;
 
 use App\Filament\Outlet\Resources\ServiceResource;
 use Filament\Actions;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 use Filament\Resources\Pages\EditRecord;
 
 class EditService extends EditRecord
@@ -15,5 +19,28 @@ class EditService extends EditRecord
         return [
             Actions\DeleteAction::make(),
         ];
+    }
+
+    public function form(\Filament\Forms\Form $form): \Filament\Forms\Form {
+        return $form->schema([
+            Section::make('Services Information')
+                ->schema([
+                    TextInput::make('name')
+                        ->required(),
+                    TextInput::make('price')
+                        ->prefix('IDR')
+                        ->numeric(),
+                    Select::make('pricing_type')
+                        ->options([
+                            'per_item' => 'Per Item',
+                            'per_kilogram' => 'Per Kilogram',
+                        ])
+                        ->native(false),
+                ])->columnSpanFull(),
+            Section::make('Description')
+                ->schema([
+                    RichEditor::make('description')
+                ])
+        ]);
     }
 }
