ca46777bc41cf283c5def64f632a31fbb0433042
billalxcode <billal.xcode@gmail.com>
2024-11-17 17:30:52 +0700

    add service resource

---
+++ app/Filament/Outlet/Resources/ServiceResource.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Filament\Outlet\Resources;
+
+use App\Filament\Outlet\Resources\ServiceResource\Pages;
+use App\Models\Service;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class ServiceResource extends Resource
+{
+    protected static ?string $model = Service::class;
+
+    protected static ?string $navigationIcon = 'tabler-heart-handshake';
+
+    protected static ?string $navigationGroup = 'Settings';
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name'),
+                TextColumn::make('price')
+                    ->money('IDR'),
+                TextColumn::make('pricing_type')
+                    ->badge()
+                    ->formatStateUsing(fn($state) => str(join(' ', explode('_', $state)))->title()),
+                TextColumn::make('description')
+                    ->html()
+                    ->limit(30)
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListServices::route('/'),
+            'create' => Pages\CreateService::route('/create'),
+            'edit' => Pages\EditService::route('/{record}/edit'),
+        ];
+    }
+}
